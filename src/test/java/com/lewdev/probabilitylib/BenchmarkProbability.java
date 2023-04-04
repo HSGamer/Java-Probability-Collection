bb1740e03b31587298639f9297f60c51f0c9842f
HSGamer <huynhqtienvtag@gmail.com>
2023-04-04 21:23:25 +0700

    use maven plugin to run jmh benchmark

--- src/test/java/com/lewdev/probabilitylib/BenchmarkProbability.java
+++ src/test/java/com/lewdev/probabilitylib/BenchmarkProbability.java
@@ -1,38 +1,15 @@
 package com.lewdev.probabilitylib;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 @Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
 public class BenchmarkProbability {
-
-	public static void main(String[] args) throws RunnerException {
-		Options opt = new OptionsBuilder()
-				.include(BenchmarkProbability.class.getSimpleName())
-				.forks(1)
-				.build();
-		
-		new Runner(opt).run();
-	}
-	
 	public int elements = 1_000;
 	
 	public int toAdd = elements + 1;
